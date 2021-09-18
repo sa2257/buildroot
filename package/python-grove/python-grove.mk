e8d7579a0a9827c992c965f1173db97f313376d7
sa2257 <sa2257@cornell.edu>
2021-09-18 08:55:51 -0400

    Grove.py builds

--- package/python-grove/python-grove.mk
+++ package/python-grove/python-grove.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-PYTHON_GROVE_VERSION = 1.0
-PYTHON_GROVE_SITE = $(call github,Seeed-Studio,grove.py,v$(PYTHON_GROVE_VERSION))
+PYTHON_GROVE_VERSION = 0.7
+PYTHON_GROVE_SITE = $(call github,sa2257,grove.py,$(PYTHON_GROVE_VERSION))
 PYTHON_GROVE_LICENSE = BSD-3-Clause
 PYTHON_GROVE_LICENSE_FILES = LICENSE.rst
 PYTHON_GROVE_SETUP_TYPE = setuptools
